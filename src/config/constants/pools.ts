c40bf3bf4c830bd8728ccf20d521dc2deda632b2
GameSwapFinance <80113172+GameSwapFinance@users.noreply.github.com>
2021-07-29 06:44:15 -0700

    Pools

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -1,7 +1,8 @@
 import { PoolConfig, QuoteToken, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
-  
+  // 0x4F3F3EA447229416A79773a288e94dC16D4d144B
+  // 0.001219512195121951
 ]
 
 export default pools
