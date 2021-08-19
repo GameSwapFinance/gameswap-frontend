a2aa4dfc2103d3653eda6333e1200cf3ade17701
GameSwapFinance <80113172+GameSwapFinance@users.noreply.github.com>
2021-08-19 06:55:03 -0700

    Add pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -1,25 +1,25 @@
 import { PoolConfig, QuoteToken, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
-  /* {
-    sousId: 2,
+   {
+    sousId: 1,
     tokenName: 'WMATIC',
-    stakingTokenName: QuoteToken.Y,
+    stakingTokenName: QuoteToken.CORN,
     stakingTokenAddress: '0xAed7fEEf34a836A5755f1C0C6CCD9968Df2d422a',
     contractAddress: {
       97: '',
       56: '',
-     137: '0x65B97c51634d49e57B7893E3142f02eB51d8125B'
+     137: '0x1d36D99319D2BC8D13dCF4d8f126Cde6a2B83C3F'
     },
     poolCategory: PoolCategory.COMMUNITY,
     projectLink: 'https://corn.farm/',
     harvest: true,
-    tokenPerBlock: '.02892',
+    tokenPerBlock: '.000888',
     sortOrder: 1,
     isFinished: false,
     tokenDecimals: 18,
-    message: '3 Day 3,570 MATIC Pool'
-  }, */ 
+    message: '3 Day MATIC Pool'
+  }, 
 ]
 
 export default pools
